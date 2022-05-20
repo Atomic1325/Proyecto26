f6a88d93f34eb2aec49d21a233eb8dfd8d353e83
Atomic1325 <93055019+Atomic1325@users.noreply.github.com>
2022-05-20 16:46:13 -0500

    Add files via upload

---
+++ Jake.js
@@ -0,0 +1,16 @@
+class Jake extends BaseClass {
+    constructor(x,y){
+      super(x,y,50,50);
+      this.image = Resultado2;
+     
+    }
+  
+    display() {
+      //this.body.position.x = mouseX;
+      //this.body.position.y = mouseY;
+  
+      super.display();
+  
+  
+  }
+  }
